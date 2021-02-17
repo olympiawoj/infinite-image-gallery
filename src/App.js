e616de594703df644d46b1bc78f2b23158cc5cff
olympiawoj <owojcik7@gmail.com>
2021-02-17 09:23:17 -0500

    Infinite Scroll

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import React, { useState, useEffect } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
 import "./App.css";
 
-const accessKey = process.env.REACT_APP_UNSPLASH_ACCESS_KEY
+const accessKey = process.env.REACT_APP_UNSPLASH_ACCESS_KEY;
 
 export default function App() {
-  const [images, setImages] = useState([])
-  
+  const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1)
+
   useEffect(() => {
-    fetch(
-      `https://api.unsplash.com/photos?client_id=${accessKey}`
-    ).then((res) => res.json())
-    .then(setImages); // cleaner than passing data
-  });
+    getPhotos()
+  }, [page]);
+
+  function getPhotos(){
+    fetch(`https://api.unsplash.com/photos?client_id=${accessKey}&page=${page}`)
+    .then((res) => res.json())
+    .then((data => {
+      setImages((images) => [...images, ...data])
+    })); // cleaner than passing data
+  }
 
   // return an error if there is no access key
-  if(!accessKey){
+  if (!accessKey) {
     return (
-      <a href="https://unsplash.com/developers" className="error">Required: Get Your Unslpash API Key First</a>
-    )
+      <a href="https://unsplash.com/developers" className="error">
+        Required: Get Your Unslpash API Key First
+      </a>
+    );
   }
 
   return (
@@ -29,13 +38,20 @@ export default function App() {
         <button>Search</button>
       </form>
 
-      <div className="image-grid">
-        {images.map((image, index) => (
-          <div className="image" key={index}>
-            <img src={image.urls.regular} alt={image.alt_description} />
-          </div>
-        ))}
-      </div>
+      <InfiniteScroll
+        dataLength={images.length}
+        next={()=>setPage((page) => page + 1)}
+        hasMore={true}
+        loader={<h4>Loading...</h4>}
+      >
+        <div className="image-grid">
+          {images.map((image, index) => (
+            <a className="image" key={index} href={image.links.html} target="_blank" rel="noopener noreferrer">
+              <img src={image.urls.regular} alt={image.alt_description} />
+            </a>
+          ))}
+        </div>
+      </InfiniteScroll>
     </div>
   );
 }
