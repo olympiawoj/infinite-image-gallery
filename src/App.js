8363ea9c4f5adb5d7979996be400a1d11763ff3c
olympiawoj <owojcik7@gmail.com>
2021-02-16 19:55:43 -0500

    Getting Images from Unsplash

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+const accessKey = process.env.REACT_APP_UNSPLASH_ACCESS_KEY
 
 export default function App() {
+  const [images, setImages] = useState([])
+  
+  useEffect(() => {
+    fetch(
+      `https://api.unsplash.com/photos?client_id=${accessKey}`
+    ).then((res) => res.json())
+    .then(setImages); // cleaner than passing data
+  });
+
+  // return an error if there is no access key
+  if(!accessKey){
+    return (
+      <a href="https://unsplash.com/developers" className="error">Required: Get Your Unslpash API Key First</a>
+    )
+  }
+
   return (
     <div className="app">
       <h1>Unsplash Image Gallery!</h1>
@@ -12,9 +30,9 @@ export default function App() {
       </form>
 
       <div className="image-grid">
-        {[...Array(100)].map((_, index) => (
+        {images.map((image, index) => (
           <div className="image" key={index}>
-            <img src="https://placekitten.com/g/1920/1080" alt="Sample" />
+            <img src={image.urls.regular} alt={image.alt_description} />
           </div>
         ))}
       </div>
